e0a88e34b099d39b3f34104a735b5e662676aa50
Janaka Kumara <janakakumara.sns@gmail.com>
2025-09-02 21:43:30 +0530

    Redeploy site with latest changes

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/janaka-kumara-portfolio-site/",
+  base: "janaka-kumara-portfolio-site/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
