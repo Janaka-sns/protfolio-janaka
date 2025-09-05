35f25558ce2fdc34cc5bfece450f8fa67960c209
Janaka Kumara <janakakumara.sns@gmail.com>
2025-09-05 23:18:02 +0530

    made changes to github pages linking areas

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: process.env.NODE_ENV === 'production' ? "/janaka-kumara-portfolio-site/" : "/",
+  base: process.env.NODE_ENV === 'production' ? "/protfolio-janaka/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
