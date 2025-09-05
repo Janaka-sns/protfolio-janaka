ba4e52ec07a826b16a9436adfd7365b1d8046db3
Janaka Kumara <janakakumara.sns@gmail.com>
2025-09-05 23:56:54 +0530

    Create minimal test app to isolate the issue
    - Replace complex components with simple test content
    - This will help identify if the issue is with specific components
    - If this works, we know the problem is in one of the portfolio components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +10,11 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <Index />
+      <div style={{ padding: '20px', backgroundColor: 'white', color: 'black' }}>
+        <h1>Test - Portfolio Site</h1>
+        <p>If you can see this, the React app is working!</p>
+        <p>Current time: {new Date().toLocaleString()}</p>
+      </div>
     </TooltipProvider>
   </QueryClientProvider>
 );
