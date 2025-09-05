d7cf8df33ad8534736e555c581291bd494db575b
Janaka Kumara <janakakumara.sns@gmail.com>
2025-09-06 00:00:42 +0530

    Test Navigation component
    - Add Navigation component to isolate which component is causing issues
    - This will help identify if Navigation is the problematic component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Navigation from '@/components/Navigation';
 
 const queryClient = new QueryClient();
 
@@ -11,9 +12,9 @@ const App = () => (
       <Toaster />
       <Sonner />
       <div style={{ padding: '20px', backgroundColor: 'white', color: 'black' }}>
-        <h1>Test - Portfolio Site</h1>
-        <p>If you can see this, the React app is working!</p>
-        <p>Current time: {new Date().toLocaleString()}</p>
+        <h1>Testing Navigation Component</h1>
+        <Navigation />
+        <p>If you can see this, Navigation component is working!</p>
       </div>
     </TooltipProvider>
   </QueryClientProvider>
